9f9507188f1ddd28cde08ccfa2ecdb6214fbc89e
Benjamin <mazlgoth@gmail.com>
2018-11-08 20:24:23 -0800

    Implemented permissions for admin tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,39 @@
 module ApplicationHelper
-	def admin_types
-		['AdminUser']
-	end
 
-	def active?(path)
-		"active" if current_page?(path)
-	end
+  def admin_types
+    ['AdminUser']
+  end
+  
+  def active?(path)
+    "active" if current_page?(path)
+  end
 
-	def employee?
-		current_user.type? == "Employee"
-	end
+  def employee?
+    current_user.type == 'Employee'
+  end
 
-	def admin?
-		admin_types.include?(current_user.type)
-	end
+  def admin?
+    admin_types.include?(current_user.type)
+  end
 
-	def status_label status
-		status_span_generator status
-	end
+  def status_label status
+    status_span_generator status
+  end
 
-	private
+  private
 
-		def status_span_generator status
-			case status
-			when 'submitted'
-				content_tag(:span, status.titleize, class: 'label label-primary')
-			when 'approved'
-				content_tag(:span, status.titleize, class: 'label label-success')
-			when 'rejected'
-				content_tag(:span, status.titleize, class: 'label label-danger')
-			when 'pending'
-				content_tag(:span, status.titleize, class: 'label label-primary')
-			when 'confirmed'
-				content_tag(:span, status.titleize, class: 'label label-success')
-			end
-		end
+  def status_span_generator status
+    case status 
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label-danger')
+    when 'pending'
+      content_tag(:span, status.titleize, class: 'label-primary')
+    when 'confirmed'
+      content_tag(:span, status.titleize, class: 'label-success')
+    end
+  end
 end
